cfd4fa44af243651a3b32c1d5787c5786e2c2ff2
sarwar1821201 <sarwardipo1821201@gmail.com>
2023-12-04 10:55:17 +0600

    run the all data into the server

--- index.js
+++ index.js
@@ -3,12 +3,15 @@ const app= express();
 
 const port= process.env.PORT || 5000;
 
+const chef=require('./data/chefs.json');
 
 app.get('/', (req,res)=>{
     res.send('our cafe is now running')
 } )
 
-
+app.get('/chefs', (req,res)=>{
+    res.send(chef)
+} )
 
 // app.listen(port, () => {
 //     console.log(`Example app listening on port ${port}`)
