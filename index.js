7f92f42659387af2b3d0c5a46102d688d2b3d223
sarwar1821201 <sarwardipo1821201@gmail.com>
2023-12-04 10:39:49 +0600

    install all server package, running the port on server

---
+++ index.js
@@ -0,0 +1,16 @@
+const express= require('express');
+const app= express();
+
+const port= process.env.PORT || 5000;
+
+app.get('/', (req,res)=>{
+    res.send('our cafe is now running')
+} )
+
+// app.listen(port, () => {
+//     console.log(`Example app listening on port ${port}`)
+//   })
+
+app.listen(port, ()=>{
+    console.log(`cafe API is running on port:${port}`)
+} )
