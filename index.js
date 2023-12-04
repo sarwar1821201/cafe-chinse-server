8002d4da75eb34aa5adc71dfe4c7b53e22c0117c
sarwar1821201 <sarwardipo1821201@gmail.com>
2023-12-04 11:04:44 +0600

    use middleware cors, so that another local server can access my server data

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express= require('express');
 const app= express();
+const cors=require('cors');
 
 const port= process.env.PORT || 5000;
 
 const chef=require('./data/chefs.json');
+
+app.use(cors());
 
 app.get('/', (req,res)=>{
     res.send('our cafe is now running')
